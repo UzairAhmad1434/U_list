c573e8a9e682fa5dffbcb3dc0b3a4b03514756a3
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-09-06 22:37:16 +0500

    todo in react

---
+++ u_list/src/TodoContext/TodoContext.js
@@ -0,0 +1,20 @@
+import { useContext, createContext } from "react";
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Uzair Ahmad",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+  updateTodo: (id, todo) => {},
+});
+
+export const useTodo = () => {
+  useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
